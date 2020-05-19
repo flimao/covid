a7a9f78ce7c685c45354f52bbce199f4eed5fd57
Felipe Lima de Oliveira <flimao@petrobras.com.br>
2020-05-19 10:55:57 -0300

    Refatorar código para orientá-lo a objetos (W.I.P)

--- scripts/covid.py
+++ scripts/covid.py
@@ -6,12 +6,14 @@
 import pandas as pd
 import os.path
 import datetime as dt
+import locale
 import seaborn as sns
 #import pymc3 as pm
 import matplotlib.pyplot as plt
 from matplotlib.ticker import LogFormatterSciNotation
 
 sns.set(style = 'ticks', rc = { 'grid.color': '.8', 'grid.linestyle': '-'})
+locale.setlocale(locale.LC_ALL,'portuguese_brazil')
 
 # ##
 # parâmetros
@@ -24,92 +26,181 @@ class CustomTicker(LogFormatterSciNotation):
     def __call__(self, x, pos=None):
         return "{x:g}".format(x=x)
 
-# leitura dos dados
 
-DATAFILE = r'HIST_PAINEL_COVIDBR_'
+class covid_brasil:
+    """
+    classe para leitura, processamento e pós-processamento dos dados brasileiros
+    da COVID-19
+    """
+    def __init__(self, diretorio = None):
 
-today = dt.date.today()
-last_day = today + dt.timedelta(days = -1)
+        # se diretorio for None, corresponde ao diretorio raiz do script
 
-DATAFILE_DATE = DATAFILE + dt.date.strftime(last_day, "%Y%m%d") + r'.xlsx'
-DATAFILE_DATA_io = os.path.join(r'.', DATAFILE_DATE)
+        if diretorio is None:
+            diretorio = r'..'
 
-covidbr = pd.read_excel(DATAFILE_DATA_io)
+        self.covidbr = self.ler_dados(diretorio)
+        self.preproc()
+        self.transform()
 
-# pré-processamento dos dados
 
-covidbr['data'] = pd.to_datetime(covidbr['data'], format='%Y-%m-%d')
+    def ler_dados(self, diretorio):
+        """
+        ler os dados da planilha excel exposta diariamente por https://covid.saude.gov.br/
 
-# ##
-# transformação dos dados
-# ##
+        :param diretorio: o diretório contendo o arquivo excel
+        :return: um dataframe contendo as informações do arquivo excel
+        """
 
-mask_exc_resumo = ~covidbr['municipio'].isnull()
+        DATAFILE = r'HIST_PAINEL_COVIDBR_'
 
-interessante = ['regiao', 'estado', 'municipio', 'data', 'dias_caso_0',
-                'obitosAcumulado', 'casosAcumulado', 'obitosNovo', 'casosNovo',
-                'obitosMMhab', 'casosMMhab', 'obitosAcumMMhab', 'casosAcumMMhab']
+        today = dt.date.today()
+        last_day = today + dt.timedelta(days = -1)
 
-agrupar_full = ['estado', 'municipio']
-agrupar_estado = ['estado']
-agrupar_regiao = ['regiao']
+        DATAFILE_DATE = DATAFILE + dt.date.strftime(last_day, "%d%b%Y") + r'.xlsx'
+        DATAFILE_DATA_io = os.path.join(diretorio, r'data', r'Brasil', DATAFILE_DATE)
 
-# dados em que a contaminação ocorreu fora de um município:
-# substituir o nome do município de NaN para 'SEM MUNICÍPIO'
+        return pd.read_excel(DATAFILE_DATA_io)
 
-# resumo por estado: substituir o nome do município de NaN para 'RESUMO'
 
-# resumo Brasil: substituir os nomes do munícipio e do estado de NaN para 'Brasil'
+    def preproc(self):
+        """
+        pre-processamento dos dados
+        :return: None
+        """
 
-mask_forademunicipios = (covidbr['municipio'].isnull() & ~covidbr['codmun'].isnull())
-mask_resumo_estado = (covidbr['municipio'].isnull() & covidbr['codmun'].isnull())
-mask_resumo_brasil = (covidbr['estado'].isnull())
+        self.covidbr['data'] = pd.to_datetime(self.covidbr['data'], format='%Y-%m-%d')
 
-covidbr.loc[covidbr[mask_forademunicipios].index, 'municipio'] = 'SEM MUNICÍPIO'
-covidbr.loc[covidbr[mask_resumo_estado].index, 'municipio'] = 'RESUMO'
-covidbr.loc[covidbr[mask_resumo_brasil].index, 'municipio'] = 'Brasil'
-covidbr.loc[covidbr[mask_resumo_brasil].index, 'estado'] = 'Brasil'
 
-# data para dias desde o 1o caso
+    def transform(self):
+        """
+        transformação dos dados. Engloba várias transformações
+        :return: None
+        """
 
-covidbr['dias_caso_0'] = covidbr['data'] - covidbr['data'].iloc[0]
-covidbr['dias_caso_0'] = covidbr['dias_caso_0'].apply(lambda x: getattr(x, 'days')).astype(int)
+        # definição de constantes
+        self.mask_exc_resumo = ~self.covidbr['municipio'].isnull()
 
-# casos diários a partir de casos acumulados (idem para obitos)
+        self.interessante = ['regiao', 'estado', 'municipio', 'data', 'dias_caso_0',
+                             'obitosAcumulado', 'casosAcumulado', 'obitosNovo', 'casosNovo',
+                             'obitosMMhab', 'casosMMhab', 'obitosAcumMMhab', 'casosAcumMMhab']
 
-covidbr['obitosNovo'] = covidbr.groupby(agrupar_full)['obitosAcumulado'].diff().fillna(0)
-covidbr['casosNovo'] = covidbr.groupby(agrupar_full)['casosAcumulado'].diff().fillna(0)
+        self.agrupar_full = ['estado', 'municipio']
+        self.agrupar_estado = ['estado']
+        self.agrupar_regiao = ['regiao']
 
-# casos e óbitos na última semana
+        # transformações
+        self.substituir_nomes()
+        self.dias_desde_caso_0()
+        self.casos_obitos_novos()
+        self.casos_obitos_ultima_semana()
+        self.casos_obitos_percapita()
+        self.suavizacao()
 
-covidbr['obitos_7d'] = covidbr.groupby(agrupar_full)['obitosAcumulado'].diff(7).fillna(0)
-covidbr['casos_7d'] = covidbr.groupby(agrupar_full)['casosAcumulado'].diff(7).fillna(0)
 
-# casos e obitos por milhão de habitantes
+    def substituir_nomes(self):
+        """
+        substituir nomes relevantes:
+            dados em que a contaminação ocorreu fora de um município:
+            substituir o nome do município de NaN para 'SEM MUNICÍPIO'
 
-covidbr['obitosMMhab'] = covidbr['obitosNovo'] / (covidbr['populacaoTCU2019']/(10**6))
-covidbr['casosMMhab'] = covidbr['casosNovo'] / (covidbr['populacaoTCU2019']/(10**6))
+            resumo por estado: substituir o nome do município de NaN para 'RESUMO'
 
-covidbr['obitosAcumMMhab'] = covidbr['obitosAcumulado'] / (covidbr['populacaoTCU2019']/(10**6))
-covidbr['casosAcumMMhab'] = covidbr['casosAcumulado'] / (covidbr['populacaoTCU2019']/(10**6))
+            resumo Brasil: substituir os nomes do munícipio e do estado de NaN para 'Brasil'
 
-covidbr['obitos_7d_MMhab'] = covidbr['obitos_7d'] / (covidbr['populacaoTCU2019']/(10**6))
-covidbr['casos_7d_MMhab'] = covidbr['casos_7d'] / (covidbr['populacaoTCU2019']/(10**6))
+        :return: None
+        """
 
-# suavização: média móvel
+        mask_forademunicipios = (self.covidbr['municipio'].isnull() & ~self.covidbr['codmun'].isnull())
+        mask_resumo_estado = (self.covidbr['municipio'].isnull() & self.covidbr['codmun'].isnull())
+        mask_resumo_brasil = (self.covidbr['estado'].isnull())
 
-mm_aplicar = ['obitosAcumMMhab', 'obitos_7d_MMhab', 'casosAcumMMhab', 'casos_7d_MMhab']
-for mm in mm_aplicar:
-    covidbr[mm + '_mm'] = covidbr.groupby(agrupar_full)[mm].apply(lambda x: x.rolling(mm_periodo).mean())
+        self.covidbr.loc[self.covidbr[mask_forademunicipios].index, 'municipio'] = 'SEM MUNICÍPIO'
+        self.covidbr.loc[self.covidbr[mask_resumo_estado].index, 'municipio'] = 'RESUMO'
+        self.covidbr.loc[self.covidbr[mask_resumo_brasil].index, 'municipio'] = 'Brasil'
+        self.covidbr.loc[self.covidbr[mask_resumo_brasil].index, 'estado'] = 'Brasil'
 
-# data para dias desde 0.1 obito por MM hab
 
-mask_obitoMMhab = covidbr['obitosAcumMMhab'] >= 0.1
+    def dias_desde_caso_0(self):
+        """
+        calcular dias desde caso 0
+        :return: None
+        """
 
-covidrel = covidbr.loc[mask_obitoMMhab,:]
+        self.covidbr['dias_caso_0'] = self.covidbr['data'] - self.covidbr['data'].iloc[0]
+        self.covidbr['dias_caso_0'] = self.covidbr['dias_caso_0'].apply(lambda x: x.days).astype(int)
 
-covidrel['dias_desde_obito_MMhab'] = covidrel.groupby(agrupar_full)['data'].apply(lambda x:x-x.iloc[0])
-covidrel['dias_desde_obito_MMhab'] = covidrel['dias_desde_obito_MMhab'].apply(lambda x: x.days).astype(int)
+
+    def casos_obitos_novos(self):
+        """
+        casos diários a partir de casos acumulados (idem para obitos)
+        :return: None
+        """
+
+        self.covidbr['obitosNovo'] = self.covidbr.groupby(self.agrupar_full)['obitosAcumulado'].diff().fillna(0)
+        self.covidbr['casosNovo'] = self.covidbr.groupby(self.agrupar_full)['casosAcumulado'].diff().fillna(0)
+
+
+    def casos_obitos_ultima_semana(self):
+        """
+        casos e óbitos na última semana
+        :return: None
+        """
+
+        self.covidbr['obitos_7d'] = self.covidbr.groupby(self.agrupar_full)['obitosAcumulado'].diff(7).fillna(0)
+        self.covidbr['casos_7d'] = self.covidbr.groupby(self.agrupar_full)['casosAcumulado'].diff(7).fillna(0)
+
+
+    def casos_obitos_percapita(self):
+        """
+        casos e óbitos por milhão de habitantes
+        :return: None
+        """
+
+        self.covidbr['obitosMMhab'] = self.covidbr['obitosNovo'] / (self.covidbr['populacaoTCU2019'] / (10 ** 6))
+        self.covidbr['casosMMhab'] = self.covidbr['casosNovo'] / (self.covidbr['populacaoTCU2019'] / (10 ** 6))
+
+        self.covidbr['obitosAcumMMhab'] = self.covidbr['obitosAcumulado'] /\
+                                          (self.covidbr['populacaoTCU2019'] / (10 ** 6))
+        self.covidbr['casosAcumMMhab'] = self.covidbr['casosAcumulado'] / (self.covidbr['populacaoTCU2019'] / (10 ** 6))
+
+        self.covidbr['obitos_7d_MMhab'] = self.covidbr['obitos_7d'] / (self.covidbr['populacaoTCU2019'] / (10 ** 6))
+        self.covidbr['casos_7d_MMhab'] = self.covidbr['casos_7d'] / (self.covidbr['populacaoTCU2019'] / (10 ** 6))
+
+
+    def suavizacao(self):
+        """
+        suavização via média móvel com período definido anteriormente
+        :return: None
+        """
+
+        mm_aplicar = ['obitosAcumMMhab', 'obitos_7d_MMhab', 'casosAcumMMhab', 'casos_7d_MMhab']
+        for mm in mm_aplicar:
+            self.covidbr[mm + '_mm'] = self.covidbr.groupby(self.agrupar_full)[mm].apply(
+                lambda x: x.rolling(mm_periodo).mean()
+            )
+
+    def dias_desde_obito_percapita(self):
+        """
+        cálculo de # de dias desde 0.1 obito por MM hab
+        :return: None
+        """
+        self.mask_obitoMMhab = self.covidbr['obitosAcumMMhab'] >= 0.1
+
+        self.covidrel = self.covidbr.loc[mask_obitoMMhab, :]
+
+        self.covidrel['dias_desde_obito_MMhab'] = self.covidrel.groupby(self.agrupar_full)['data'].apply(
+            lambda x: x - x.iloc[0]
+        )
+
+        self.covidrel['dias_desde_obito_MMhab'] = self.covidrel['dias_desde_obito_MMhab'].apply(
+            lambda x: x.days
+        ).astype(int)
+
+
+br = covid_brasil(diretorio = None)
+covidrel = br.covidrel
+
 
 # ##
 # pos-processamento
@@ -117,7 +208,7 @@ covidrel['dias_desde_obito_MMhab'] = covidrel['dias_desde_obito_MMhab'].apply(la
 
 # grafico obitos acumulados por MM hab(log) x novos obitos na ultima semana por MM hab (log)
 plt.figure()
-ax1o = sns.lineplot(data = covidrel[~mask_exc_resumo][covidrel['estado'].isin(['RJ', 'SP', 'AM', 'RS', 'Brasil'])],
+ax1o = sns.lineplot(data = covidrel[~br.mask_exc_resumo][covidrel['estado'].isin(['RJ', 'SP', 'AM', 'RS', 'Brasil'])],
                   x = 'obitosAcumMMhab_mm', y = 'obitos_7d_MMhab_mm', hue='estado',
                   err_style=None)
 plt.tight_layout()
@@ -132,7 +223,7 @@ ax2o = sns.lineplot(data = covidrel[covidrel['municipio'].isin(
 plt.tight_layout()
 sns.despine()
 
-ax1c = sns.lineplot(data = covidrel[~mask_exc_resumo][covidrel['estado'].isin(['RJ', 'SP', 'AM', 'RS', 'Brasil'])],
+ax1c = sns.lineplot(data = covidrel[~br.mask_exc_resumo][covidrel['estado'].isin(['RJ', 'SP', 'AM', 'RS', 'Brasil'])],
                   x = 'casosAcumMMhab_mm', y = 'casos_7d_MMhab_mm', hue='estado',
                   err_style=None)
 plt.tight_layout()
