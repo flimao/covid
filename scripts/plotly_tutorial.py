670da685fdf90d44056bb2e6218398741cf14d0d
Felipe Lima de Oliveira <flimao@petrobras.com.br>
2020-06-03 19:57:46 -0300

    Plotly + Dash tutorial. Inúmeras pequenas modificações para testar controles do Dash.

--- scripts/plotly_tutorial.py
+++ scripts/plotly_tutorial.py
@@ -32,12 +32,18 @@ mm_periodo = 5
 
 #data_estados = ['AM', 'SP', 'RJ', 'Brasil']
 data_estados = ['Brasil', 'RJ']
+data_municipios = ['Brasil', 'Niterói', 'Rio de Janeiro']
 normalizacao = ['percapita']
 
 br = covid.covid_brasil(diretorio = None, graficos = False)
 df = br.covidrel[(~br.mask_exc_resumo_rel)&(br.covidrel['estado'].isin(data_estados))]
+df_est = br.covidrel[br.mask_exc_resumo_rel]
+df_estunq = df_est.groupby('coduf')['estado'].first()
 
-df_norm, titulo_x, titulo_y = br.norm_grafico(
+df_mun = br.covidrel[br.mask_exc_resumo_rel]
+df_mununq = df_mun.groupby('codmun')[['estado', 'municipio']].first()
+
+df_norm, titulo, _ = br.norm_grafico(
     dados=df,
     normalizacao=normalizacao,
     x_orig='dias_desde_obito_MMhab',
@@ -64,62 +70,49 @@ fig.update_traces(connectgaps = True,
 fig.update_layout(hovermode='x unified',
                   title_text = 'Evolução da COVID-19 no Brasil (Óbitos)')
 
-fig.update_yaxes(title_text = titulo_y['y_ott'])
-fig.update_xaxes(title_text = titulo_x['x_ott'])
+fig.update_yaxes(title_text = titulo['y_ont'])
+fig.update_xaxes(title_text = titulo['x_ont'])
 
 log_linear = [{
     'active': 0,
-    'y': 0.96, 'x': -0.11,
-    'xanchor': 'right', 'yanchor':'top',
+    'y': 1, 'x': 0,
+    'xanchor': 'left', 'yanchor':'top',
     'type': 'dropdown',
     'buttons':[
         { 'label': 'Log',
           'method': 'relayout',
           'args': ['yaxis', {'type': 'log',
-                             'title': {'text': titulo_y['y_ott'] }}]
+                             'title': {'text': titulo['y_ont'] }}]
         },
         { 'label': 'Linear',
           'method': 'relayout',
           'args': ['yaxis', {'type': 'linear',
-                             'title': {'text': titulo_y['y_ott'] }}]
+                             'title': {'text': titulo['y_ont'] }}]
         }
 ]
 },{
     'active': 1,
-    'y': -0.16, 'x': 0.9,
-    'xanchor': 'left', 'yanchor':'top',
+    'y': 0, 'x': 1,
+    'xanchor': 'right', 'yanchor':'bottom',
     'type': 'dropdown', 'direction': 'left',
     'buttons':[
         { 'label': 'Log',
           'method': 'relayout',
           'args': ['xaxis', {'type': 'log',
-                             'title': {'text': titulo_x }}]
+                             'title': {'text': titulo['x_ont'] }}]
         },
         { 'label': 'Linear',
           'method': 'relayout',
           'args': ['xaxis', {'type': 'linear',
-                             'title': {'text': titulo_x }}]
+                             'title': {'text': titulo['x_ont'] }}]
         }
 ]
 }]
 
-annot_log_linear = [
-    dict(text="Escala", showarrow=False,
-         x=-0.11, y=0.96, yref="paper", xref='paper',
-         xanchor='right', yanchor='bottom',
-        font_size=16
-         ),
-    dict(text="Escala", showarrow=False,
-         x=0.9, y=-0.17, yref="paper", xref='paper',
-         xanchor='right', yanchor='top',
-         font_size=16
-         )
-]
-
 obitos_casos = [dict(
     active=0,
     x=0.5, y=1.1,
-    xanchor='left', yanchor='top',
+    xanchor='left', yanchor='bottom',
     type='dropdown', direction='down',
     buttons=[dict(
         label='óbitos',
@@ -131,8 +124,10 @@ obitos_casos = [dict(
         label='casos',
         method='restyle',
         args=[{'x': [ df_norm[df_norm['estado'] == c].x_ctt for c in data_estados ],
-               'y': [ df_norm[df_norm['estado'] == c].y_ctt for c in data_estados ]
-               }]
+               'y': [ df_norm[df_norm['estado'] == c].y_ctt for c in data_estados ],
+               'xaxis':{'title':{'text': titulo['x_cnt']}},
+               'yaxis':{'title':{'text': titulo['y_cnt']}},
+        }]
     )
     ]
 )]
@@ -140,7 +135,7 @@ obitos_casos = [dict(
 annot_obitos_casos = [
     dict(text="Tipo", showarrow=False,
          x=0.5, y=1.09, yref="paper", xref='paper',
-         xanchor='right', yanchor='top',
+         xanchor='right', yanchor='bottom',
          font_size = 16
          )
 ]
@@ -148,7 +143,7 @@ annot_obitos_casos = [
 total_novos = [dict(
     active=0,
     x=0.75, y=1.1,
-    xanchor='left', yanchor='top',
+    xanchor='left', yanchor='bottom',
     type='dropdown', direction='down',
     buttons=[dict(
         label='# total',
@@ -165,14 +160,14 @@ total_novos = [dict(
 annot_total_novos = [
     dict(text="Concentração", showarrow=False,
          x=0.75, y=1.09, yref="paper", xref='paper',
-         xanchor='right', yanchor='top',
+         xanchor='right', yanchor='bottom',
          font_size = 16
          )
 ]
 
-fig.update_layout(updatemenus=log_linear + obitos_casos + total_novos,
-                  annotations=annot_log_linear + annot_obitos_casos + annot_total_novos)
-
+fig.update_layout(updatemenus=log_linear# + obitos_casos + total_novos,
+                  #annotations=annot_obitos_casos + annot_total_novos)
+                  )
 fig.write_html(html_fig)
 fig.write_image(img)
 
@@ -185,12 +180,47 @@ app.layout = html.Div(children=[
 
     html.Div(children='''
         Uma análise de dados da COVID-19 do Brasil e do mundo.
+        \n\n\n        
     '''),
 
     dcc.Graph(
         id='covid',
         figure=fig
-    )
+    ),
+
+    html.Div(children=[
+
+        html.Label('Municípios'),
+        dcc.Dropdown(
+            options=[
+                {'label': df_mununq.loc[codmun, 'municipio'] + ', ' + df_mununq.loc[codmun, 'estado'], 'value': codmun}
+                for codmun in df_mununq.index
+            ],
+            value=[330330, 330445],
+            multi=True
+        ),
+
+        html.Label('Estados'),
+        dcc.Dropdown(
+            options=[
+                {'label': df_estunq[coduf], 'value': coduf} for coduf in df_estunq.index
+            ],
+            value=[33, 34, 76],
+            multi=True
+        ),
+
+        html.Label('Suavização'),
+        dcc.RadioItems(
+            options=[
+                {'label': 'Sem suavização', 'value': 0},
+                {'label': 'Média móvel de 7 dias', 'value': 7},
+                {'label': 'Média móvel de 3 dias', 'value': 3},
+                {'label': 'Outra média móvel', 'value': -1}
+            ],
+            value=7
+        ),
+        dcc.Input(value=5, type='number'),
+    ], style={'columnCount': 2})
 ])
 
 if __name__ == '__main__':
--- scripts/webapp/dash_tutorial.py
+++ scripts/webapp/dash_tutorial.py
@@ -2,34 +2,38 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
+from dash.dependencies import Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash!'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-    '''),
-
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
+app.layout = html.Div([
+    dcc.Input(
+        id='num-multi',
+        type='number',
+        value=5
+    ),
+    html.Table([
+        html.Tr([html.Td(['x', html.Sup(2)]), html.Td(id='square')]),
+        html.Tr([html.Td(['x', html.Sup(3)]), html.Td(id='cube')]),
+        html.Tr([html.Td([2, html.Sup('x')]), html.Td(id='twos')]),
+        html.Tr([html.Td([3, html.Sup('x')]), html.Td(id='threes')]),
+        html.Tr([html.Td(['x', html.Sup('x')]), html.Td(id='x^x')]),
+    ]),
 ])
 
-app.layout['example-graph'].figure = go.Figure(data=go.Scatter(x=[1,2,3,4], y=[4,2,3,1]), layout_title_text='Yay')
+
+@app.callback(
+    [Output('square', 'children'),
+     Output('cube', 'children'),
+     Output('twos', 'children'),
+     Output('threes', 'children'),
+     Output('x^x', 'children')],
+    [Input('num-multi', 'value')])
+def callback_a(x):
+    return x**2, x**3, 2**x, 3**x, x**x
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
